a54f4cf8ea4edda04fd8d4d05b0c1978762bd341
WanJie <webmaster@wanjie.info>
2020-02-14 17:40:45 +0800

    code with github
    code with github for daocloud nano

---
+++ daomonit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Author: jie.wan@daocloud.io
+# Created at 2020-02-14
+# Usage: .curl https://webmaster.coding.net/p/t/d/t/git/raw/master/README.md/daomonit.sh | bash
+
+# part1,解决面板找不到主机，提示“连接超时，请刷新重联” 问题
+apt-get update --fix-missing
+apt-get install ca-certificates -y
+service daomonit restart
+
+# part2,解决x509问题
+echo -n | openssl s_client -showcerts -connect daocloud.io:443 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /usr/local/share/ca-certificates/ca.crt
+update-ca-certificates
+service docker restart
+
+#part3,下载dao-2048测试
+docker pull daocloud.io/daocloud/dao-2048:latest
